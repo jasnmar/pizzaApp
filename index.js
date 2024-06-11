ea087b6601db5300034d3c19ee778d37f27f97d7
jasnmar <jasnmar@gmail.com>
2024-06-11 13:13:38 -0400

    placeOrder function

--- index.js
+++ index.js
@@ -5,9 +5,29 @@ const menu = [
   {name: "Veggie", price: 9},
 ]
 
-const cashInRegister = 100
+let cashInRegister = 100
 const orderQueue = []
 
 function addNewPizza(pizza) {
   menu.push(pizza)
 } 
+
+/**
+ * Write another utility function, placeOrder, that takes a pizza name parameter and:
+ * 1. finds that pizza object in the menu,
+ * 2. adds the income to the cashInRegister,
+ * 3. pushes a new "order object" to the orderQueue 
+ *    (e.g. { pizza: selectedPizzaObjectFromStep1, status: "ordered" })
+ * 4. returns the new order object (just in case we need it later)
+ */
+
+function placeOrder(pizzaName) {
+  const pizzaObject = menu.find((item) => item.name === pizzaName)
+  cashInRegister += pizzaObject.price
+  pizzaObject.status = "ordered"
+  orderQueue.push(pizzaObject)
+  console.log('pizzaObject: ', pizzaObject)
+  return pizzaObject
+}
+
+placeOrder("Pepperoni")
